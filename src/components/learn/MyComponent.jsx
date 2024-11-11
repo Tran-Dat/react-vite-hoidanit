ba782528b0c2b4bf2af65940fd77ceec13b52a80
Tran Dat <hoclaptrinhweb001@example.com>
2024-11-11 22:01:03 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,13 @@
 import "./style.css";
 
 const MyComponent = () => {
+  const hoidanit = [1, 2, 3];
+  //   const hoidanit = { name: "hoidanit", age: 25 };
+
   return (
     <>
-      <div> Dat & Hoi Dan IT</div>
+      <div>{JSON.stringify(hoidanit)} Dat & Hoi Dan IT</div>
+      <div>{console.log("Erik")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
