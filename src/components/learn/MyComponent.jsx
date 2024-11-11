d80c61573954cb540e8a387149e27668eedd85d2
Tran Dat <hoclaptrinhweb001@example.com>
2024-11-11 21:43:34 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,14 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div> Dat & Hoi Dan IT</div>;
+  return (
+    <>
+      <div> Dat & Hoi Dan IT</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;
