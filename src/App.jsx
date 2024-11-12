a32f3df9032fafa41e4b08c71f5be9eaeec96837
Tran Dat <hoclaptrinhweb001@example.com>
2024-11-12 16:28:25 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import TodoNew from "./components/todo/todoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+  const hoidanit = "eric MU";
+  const age = 25;
+  const data = {
+    address: "HCM",
+    country: "VN",
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData name={hoidanit} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  const { name, age, data } = props;
+
   return (
     <div className="toda-data">
+      <div>My naem is a: {name}</div>
       <div> Learning React</div>
       <div> Learning React</div>
     </div>
