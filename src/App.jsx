e6225a752f90c3610bee6490afe61a9b036bdc7a
Tran Dat <hoclaptrinhweb001@example.com>
2024-11-12 16:44:54 +0700

    video 35

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import TodoNew from "./components/todo/todoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
-  const hoidanit = "eric MU";
+  const name = "eric MU";
   const age = 25;
   const data = {
     address: "HCM",
     country: "VN",
   };
 
+  const addNewTodo = (name) => {
+    alert(`call me ${name}`);
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoNew />
-      <TodoData name={hoidanit} age={age} data={data} />
+      <TodoNew addNewTodo={addNewTodo} />
+      <TodoData name={name} age={age} data={data} />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -1,4 +1,9 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+  console.log(props, "check");
+
+  const { addNewTodo } = props;
+
+  // addNewTodo("eric");
   return (
     <div className="todo-new">
       <input type="text" className="input" placeholder="Enter your task" />
