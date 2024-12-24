3f44cb70f7a01821654dfd706a07a98315827888
Tran Dat <hoclaptrinhweb001@example.com>
2024-12-24 12:45:33 +0700

    video 84

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,8 +1,13 @@
-import { Button, Drawer } from "antd";
+import { Button, Drawer, notification } from "antd";
 import { useState } from "react";
+import {
+  handleUploadFile,
+  updateUserAvatarAPI,
+} from "../../services/api.services";
 
 const ViewUserDetail = (props) => {
-  const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
+  const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen, loadUser } =
+    props;
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [preview, setPreview] = useState(null);
@@ -16,11 +21,48 @@ const ViewUserDetail = (props) => {
 
     const file = event.target.files[0];
     if (file) {
-      setSelectedFile;
+      setSelectedFile(file);
       setPreview(URL.createObjectURL(file));
     }
   };
-  console.log(">>check file: ", preview);
+  const handleUpdateUserAvatar = async () => {
+    // step 1: upload file
+    const resUpload = await handleUploadFile(selectedFile, "avatar");
+    if (resUpload.data) {
+      // success
+      const newAvatar = resUpload.data.fileUploaded;
+      const resUpdateAvatar = await updateUserAvatarAPI(
+        newAvatar,
+        dataDetail._id,
+        dataDetail.fullName,
+        dataDetail.phone
+      );
+      if (resUpdateAvatar.data) {
+        setIsDetailOpen(false);
+        setSelectedFile(null);
+        setPreview(null);
+        await loadUser();
+
+        notification.success({
+          message: "Update user avatar",
+          description: "Cap nhat avatar thanh cong",
+        });
+      } else {
+        notification.error({
+          message: "Error update avatar",
+          description: JSON.stringify(resUpdateAvatar.message),
+        });
+      }
+    } else {
+      // false
+      notification.error({
+        message: "Error Upload File",
+        description: JSON.stringify(resUpload.message),
+      });
+    }
+    // step 2: upload user
+  };
+
   return (
     <>
       <Drawer
@@ -82,23 +124,32 @@ const ViewUserDetail = (props) => {
               />
             </div>
             {preview && (
-              <div
-                style={{
-                  marginTop: "10px",
-                  height: "100px",
-                  width: "150px",
-                  border: "1px solid #ccc",
-                }}
-              >
-                <img
+              <>
+                <div
                   style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "contain",
+                    margin: "10px 0px 15px",
+                    height: "100px",
+                    width: "150px",
                   }}
-                  src={preview}
-                />
-              </div>
+                >
+                  <img
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
+                    }}
+                    src={preview}
+                  />
+                </div>
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    handleUpdateUserAvatar();
+                  }}
+                >
+                  Save
+                </Button>
+              </>
             )}
           </>
         ) : (
